37e7143dbe8486ba414dc3c9ab1ba94e5e0658c9
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-10-29 17:07:29 +0200

    Bump year and version

--- lib/Plack/Middleware/Proxy/Connect/IO.pm
+++ lib/Plack/Middleware/Proxy/Connect/IO.pm
@@ -37,7 +37,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.0100';
+our $VERSION = '0.0200';
 
 
 use parent qw(Plack::Middleware);
@@ -125,7 +125,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2014 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2014, 2016 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
